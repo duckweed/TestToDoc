72ac8273e98d4df0c4159f9f3af50b672c60eeb0
duckweed <andrew.oxenburgh@gmnail.com>
2012-03-08 07:11:04 +1300

    now walks through jar listing annotations.

---
+++ TestToDoc-TestJar/src/test/java/org/adscale/testtodoc/testjar/AppTest.java
@@ -0,0 +1,12 @@
+package org.adscale.testtodoc.testjar;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
