0c2b1e629e8193f062359ac2025563e4bdcbb439
duckweed <andrew.oxenburgh@gmnail.com>
2012-03-15 06:57:43 +1300

    added licence text. added more usage.

--- TestToDoc-Main/src/test/java/org/adscale/testtodoc/Main_Test.java
+++ TestToDoc-Main/src/test/java/org/adscale/testtodoc/Main_Test.java
@@ -9,12 +9,28 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Copyright 2012 Andrew Oxenburgh
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 public class Main_Test extends Main {
 
     @Test
     public void usage() throws Exception {
         main(new String[]{});
-        String expected = "usage TestToDoc: jarFileName".trim();
+        String expected = "usage TestToDoc: jarFileName" + LN_SEP + " -b : break between classes";
         assertSystemOut(expected);
     }
 
